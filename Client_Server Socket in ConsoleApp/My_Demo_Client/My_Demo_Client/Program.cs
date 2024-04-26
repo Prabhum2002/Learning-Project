20ade3e0e977dca71c705bf30f308753c53667fe
Prabhum2002 <shamugaprabhum@gmail.com>
2024-04-26 19:18:27 +0530

    Initial Changes

---
+++ Client_Server Socket in ConsoleApp/My_Demo_Client/My_Demo_Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+//Client Code
+
+//Info about localhost
+IPHostEntry ipEntry = await Dns.GetHostEntryAsync(Dns.GetHostName());
+
+//localhost ip address----127.0.1.1
+IPAddress ip= ipEntry.AddressList[0];
+
+IPEndPoint iPEndPoint = new(ip,1234);
+
+//client socket
+using Socket client = new(
+    iPEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp
+    );
+await client.ConnectAsync(iPEndPoint);
+
+while (true) {
+    Console.Write("Send a Message: ");
+    var message=Console.ReadLine();
+
+    //convert the string message to bytes message
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+
+    //sends the message to the server
+    await client.SendAsync(messageBytes,SocketFlags.None);
+
+    var buffer = new byte[1_024];
+
+    //we received the message in the form of bytes
+    var received = await client.ReceiveAsync(buffer,SocketFlags.None);
+
+    var messageString = Encoding.UTF8.GetString(buffer,0,received);
+
+    Console.WriteLine(messageString);
+
+}
---
+++ Client_Server Socket in ConsoleApp/My_Demo_Server/My_Demo_Server/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+    //Server Code
+    //Info about our localhost --includes the ip address
+    IPHostEntry ipEntry=await Dns.GetHostEntryAsync(Dns.GetHostName());
+
+//we will extract the local host ip
+IPAddress ip = ipEntry.AddressList[0];
+
+//Connects the server socket to client socket
+IPEndPoint iPEndPoint = new(ip,1234);
+
+using Socket server = new(
+    iPEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp
+    );
+
+server.Bind(iPEndPoint);
+server.Listen();
+Console.WriteLine("Server Started Listening on port: 1234");
+
+var handler = await server.AcceptAsync();
+
+while (true) {
+    var buffer = new byte[1_024];
+    //receive the message from client but as Bytes
+    var received = await handler.ReceiveAsync(buffer,SocketFlags.None);
+    //convert bytes to string message
+    var messageString = Encoding.UTF8.GetString(buffer,0,received);
+
+    if (messageString != null) { 
+        Console.WriteLine("Message from client: {0}",messageString);
+        var response = "Message Received!";
+
+        var responseByte = Encoding.UTF8.GetBytes(response);
+
+        await handler.SendAsync(responseByte,SocketFlags.None);
+    
+    }
+
+
+
+
+
+}
+
+
